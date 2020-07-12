55fa1c3eaed437204c27bbf37e2a5442366c431a
Angela-OH <wisdom990629@likelion.org>
2020-07-13 05:09:59 +0900

    source code

--- add-nbo.cpp
+++ add-nbo.cpp
@@ -1,18 +1,8 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <netinet/in.h>
 
-
-uint32_t my_htonl(uint32_t n)
-{
-	uint32_t n1=(n & 0xFF000000) >> 24;
-	uint32_t n2=(n & 0x00FF0000) >> 8;
-	uint32_t n3=(n & 0x0000FF00) << 8;
-	uint32_t n4=(n & 0x000000FF) << 24;
-	return n1 | n2 | n3 | n4;
-}
-
-int main(int argc, char *argv[])
-{	
+int main(int argc, char* argv[]){
 	// error handling
 	if (argc!=3){
 		printf("Usage: %s <filename1> <filename2>\n", argv[0]);
@@ -36,8 +26,8 @@ int main(int argc, char *argv[])
 	uint32_t* p2=reinterpret_cast<uint32_t*>(network_buffer2);
 	
 	// invert byte order
-	uint32_t n1=my_htonl(*p1);
-	uint32_t n2=my_htonl(*p2);
+	uint32_t n1=htonl(*p1);
+	uint32_t n2=htonl(*p2);
 
 	// result print
 	printf("%d(0x%x)", n1, n1);
